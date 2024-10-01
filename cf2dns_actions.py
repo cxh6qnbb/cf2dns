243a87cb8344e7beda175c1b2922df1da03e066a
cxh6qnbb <65121634+cxh6qnbb@users.noreply.github.com>
2024-10-01 15:17:28 +0800

    Update cf2dns_actions.py

--- cf2dns_actions.py
+++ cf2dns_actions.py
@@ -4,8 +4,7 @@ import os
 
 def main():
     # 获取并输出KEY值
-    key = os.environ.get("KEY", "未设置KEY")
-    print("KEY内容:", key)
+    print(os.environ["KEY"] )
 
 if __name__ == '__main__':
     main()
